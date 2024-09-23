d66c1f1be95811e71ab99921f89619b151d4701c
TaekJun Jung <84218456+iamtaekjun@users.noreply.github.com>
2024-09-23 16:45:45 +0900

    Title: 점수 입출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240923/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        System.out.println("Your score is " + n + " point.")
+    }
+}
