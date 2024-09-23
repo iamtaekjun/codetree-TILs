eb28f90cfb539a3f09313f485f791f02201cbe5e
TaekJun Jung <84218456+iamtaekjun@users.noreply.github.com>
2024-09-23 16:09:06 +0900

    Title: 달에서 무게 구하기, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240923/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int chooWeight = 13;
+        double moonGravity = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", chooWeight, moonGravity, chooWeight*moonGravity);
+    }
+}
