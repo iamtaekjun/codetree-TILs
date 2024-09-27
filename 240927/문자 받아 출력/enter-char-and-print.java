abf7a35c5ddf6320e6d43fdd31c8f68c77b5ae9c
TaekJun Jung <84218456+iamtaekjun@users.noreply.github.com>
2024-09-27 21:58:58 +0900

    Title: 문자 받아 출력, Time: 204ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        char c = a.charAt(0);
+
+        System.out.println(c);
+    }
+}
