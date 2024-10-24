28f4b55271d6695daf428b812fe5cf872182bef0
TaekJun Jung <84218456+iamtaekjun@users.noreply.github.com>
2024-10-24 20:18:51 +0900

    Title: 전화번호 바꾸기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241024/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("-");
+    
+        int asdasd = scanner.nextInt();
+        int xxxx = scanner.nextInt();
+        int yyyy = scanner.nextInt();
+
+        System.out.println("010-" + yyyy + "-" + xxxx);
+
+    }
+}
